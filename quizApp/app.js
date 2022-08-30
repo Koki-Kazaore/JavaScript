8ccd1daf37251acda10276efe2678df09724d45c
ka300-g <kazaore@icloud.com>
2022-08-30 23:40:24 +0900

    1:36:42

--- quizApp/app.js
+++ quizApp/app.js
@@ -9,15 +9,25 @@ const correct = "かざおれ";
 
 document.getElementById("js-question").textContent = question;
 
-for(let index = 0; index < answers.length; index++){
-  document.getElementsByTagName("button")[index].textContent = answers[index];
+// クイズの選択肢を設定する関数定義
+const setupChoices = (i) => {
+  document.getElementsByTagName("button")[i].textContent = answers[i];
+}
 
-  // ボタンをクリックしたら正誤判定
-  document.getElementsByTagName("button")[index].addEventListener("click", () => {
-    if(correct === document.getElementsByTagName("button")[index].textContent){
+// ボタンをクリックしたら正誤判定する関数
+const clickHandler = (i) => {
+  document.getElementsByTagName("button")[i].addEventListener("click", (e) => {
+    if(correct === document.getElementsByTagName("button")[i].textContent){
       window.alert("正解!");
     }else{
       window.alert("不正解!");
     }
   });
+}
+
+for(let index = 0; index < answers.length; index++){
+  // 関数呼び出し
+  setupChoices(index);
+
+  clickHandler(index);
 }
